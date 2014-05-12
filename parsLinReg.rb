cc73f3760eaec6ee6be970db41434fed0601d061
Christopher Sauer <christopher.sauer@me.com>
2014-05-12 09:34:45 +0200

    parsing linReg works, with functionfile generation enabled

--- parsLinReg.rb
+++ parsLinReg.rb
@@ -93,16 +93,20 @@ if $choiceMade
     countIn = 1
 
     # write first line
-    eqs.printf(eqName + " = ")
+    equationString = eqName + " = "
+    eqs.printf(equationString)
 
     # Do for each line
     inp.each do |line|
-        eqs.printf(line[0..-3])
+        equationString << (line[0..-3] + " ")
         countIn += 1
     end
 
     # silence output
-    eqs.printf(";")
+    equationString << ";"
+
+    # write in output
+    eqs.puts(equationString)
 
     # write to files
     puts "2: Writing to Files..."
@@ -111,4 +115,112 @@ if $choiceMade
 
     # Generate output
     puts "2: Equation written to " + eqsName
+
+
+    # genMatFun
+    #==========================================================================
+
+    # Files ready for Work
+    # equation.m <= parsEquation, genMatFun
+    # out1.txt <= preParser
+
+    # varChar-variable for scanning for Variables
+    puts "3: Generating matlab functions"
+
+    # Files
+    inName = "equation.m"
+
+    # Generate and open Files
+    inp = File.open(inName, "r+")
+
+    # reset Linecounter
+    countIn = 1
+    countOut = 1
+
+    # Variable Array for your variables
+    vars = []
+    nVars = 0;
+
+    # Infos for the user
+    puts "3: Starting variable search"
+
+    # Dododo
+    inp.each do |line|
+        lineFromX = line
+        while lineFromX[varChar]
+            # Where is the Variable?
+            pos = lineFromX.index(varChar)
+            # the first cut is the deepest...
+            lineFromX = lineFromX[pos..-1]
+            # Search for next blank
+            posBlank = lineFromX.index(" ")
+            # Save Variables
+            vars[nVars] = lineFromX[0..posBlank]
+            nVars += 1
+            # the second cut
+            lineFromX = lineFromX[posBlank..-1]
+        end
+    end
+
+    # Remove duplicate variables
+    vars = vars.uniq
+    if vars.length == 0
+        puts "3: no Variables found!"
+    else
+        puts "3: " + vars.length.to_s + " Variable(s) found!"
+    end
+
+    # Set name of your function for matlab
+    printf "3: Please enter your desired functionName for Matlab (e.g. myfun): "
+
+    # Generate file for function
+    functionName = gets.chomp
+    outName = functionName + ".m"
+
+    # Opening
+    out = File.open(outName, "w")
+
+    # print to file
+    # first line
+    out.printf "function [" + eqName +"] = " + functionName + "("
+    for i in 0..(vars.length - 1)
+        out.printf vars[i]
+        if i < vars.length - 1
+            out.printf ","
+        end
+    end
+    out.printf ")"
+    out.printf "\n"
+    countOut += 1
+    # varDef
+    for i in 0..(vars.length - 1)
+        out.puts vars[i] + " = " + vars[i] + ";"
+        countOut += 1
+    end
+    # insert equation
+    out.puts(equationString)
+
+    # endfunction
+    out.puts("end")
+    countOut += 1
+
+    # *sigh* Filehandling...
+    inp.close
+    out.close
+
+    # Generating output
+    puts "3: Functionfile " + outName + " and " + countOut.to_s + " lines written"
+
+    # Information for the user
+    puts "3: Now use your generated Functionfile " + outName + " for simple Matlab Execution without variable handling for Matlab"
+
+    # Ask if unused files should be deleted
+    print "3: Should unused files be deleted? (y,n) "
+    answer = gets.chomp
+
+    if answer == "y"
+        # Removing unused files
+        File.delete("out1.txt")
+        File.delete("out2.txt")
+    end
 end
