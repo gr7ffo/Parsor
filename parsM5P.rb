a55b3d4c98ea2cac8c314d672c81a83d12c4b981
Christopher Sauer <christopher.sauer@me.com>
2014-05-12 10:24:00 +0200

    m5rules work to generate logic, working on ifElser now

--- parsM5P.rb
+++ parsM5P.rb
@@ -67,7 +67,7 @@ if $choiceMade
     # Close files
     inp.close
     out.close
-    
+
     # Generate output
     puts "1: Outputfile " + outName + " and " + countOut.to_s + " lines written"
 
@@ -198,7 +198,7 @@ if $choiceMade
     # out1.txt <= preParser
     # out2.txt <= ()remover
 
-    # add ':'
+    # file handling
     inName = "out2.txt"
     outName = "out3.txt"
 
--- parsM5Rules.rb
+++ parsM5Rules.rb
@@ -53,7 +53,7 @@ if $choiceMade
     inp.each do |line|
         # remove first line
         if countIn > 4
-            out.puts(line)
+            out.puts(line.tr(':|%','').to_s)
             countOut +=1
         end
         countIn += 1
@@ -65,4 +65,160 @@ if $choiceMade
 
     # Generate output
     puts "1: Outputfile " + outName + " and " + countOut.to_s + " lines written"
+
+
+    # []remover
+    #==========================================================================
+
+    puts "2: Removing [...]"
+
+    # Files ready for Work
+    # out1.txt <= preParser
+    # out2.txt <= []remover
+
+    # file handling
+    inName = "out1.txt"
+    outName = "out2.txt"
+
+    # reset Linecounter
+    countIn = 1
+    countOut = 1
+
+    # Generate and open Files
+    inp = File.open(inName, "r+")
+    out = File.open(outName, "w")
+
+    # cut line from '['
+    inp.each do |line|
+        if line["["]
+            newline = line[0..(line.index('[') - 1)]
+            out.puts(newline)
+            countOut += 1
+        else
+            out.puts(line)
+            countOut += 1
+        end
+            countIn +=1
+    end
+
+    # *sigh* Filehandling...
+    inp.close
+    out.close
+
+    # Generate Output
+    puts "2: Outputfile " + outName + " and " + countOut.to_s + " lines written"
+
+
+    # parsEquation
+    #==========================================================================
+
+    # Files ready for Work
+    # out1.txt <= preParser
+    # out2.txt <= []remover
+    puts "3: Parsing Equations"
+
+    # Searchterm for Equations
+    printf "3: Enter your prediction(label) (e.g. UmformgradMAX): "
+    searchTerm = gets.chomp
+    searchTermFound = false
+    lineSearchTermFound = 0
+
+    # Equationterm
+    eqName = "Rule"
+
+    # Filenames input
+    inName = "out2.txt"
+    eqsName = "equations.m"
+
+    # Files
+    inp = File.open(inName, "r+")
+    eqs = File.open(eqsName, "w")
+
+    # Linecounter Inputfile
+    countIn = 1
+    # Equationcounter
+    countEq = 1
+
+    # Stringsave
+    addString = ""
+
+    # Equationnumber
+    eqNum = 1
+
+    # Equationswrite started yet?
+    startEquations = false
+
+    # Do for each line
+    inp.each do |line|
+        if line[searchTerm]
+            searchTermFound = true
+            lineSearchTermFound = countIn
+            addString = eqName + eqNum.to_s + " = "
+            # Number your Equations
+            eqNum += 1
+        elsif searchTermFound
+            addString << line.to_s
+            if line.length == 1
+                searchTermFound = false
+                # Output
+                eqs.printf(addString.delete "\n")
+                # silence console out in Matlab
+                eqs.printf(";")
+                eqs.printf("\n")
+                countEq += 1
+                addString = ""
+            end
+        end
+        countIn += 1
+    end
+
+    # write to files
+    puts "3: Writing to Files..."
+    inp.close
+    eqs.close
+    # reopen input
+    inp = File.open(inName, "r+")
+    # Generate output
+    puts "3: " + eqNum.to_s + " Equation(s) written to " + eqsName
+
+    # cut file to logic from out2.txt, leads to out3.txt
+    # But first let me (take) save a (sel)fi(l)e
+    inp.close
+    outName = "out3.txt"
+    inp = File.open(inName, "r+")
+    out = File.open(outName, "w")
+
+    # reset Linecounter
+    countIn = 1
+    countOut = 1
+    i = 0
+    # Write to outputfile
+    inp.each do |line|
+        if line[searchTerm]
+            searchTermFound = true
+            lineSearchTermFound = countIn
+        elsif searchTermFound
+            if line.length == 1
+                searchTermFound = false
+            end
+        elsif line["IF"]
+            # do nothing
+        elsif line["THEN"]
+            # do nothing
+        elsif line["Rule"]
+            # do nothing
+        else
+            # put line, only if there is logic
+            out.puts(line)
+            countOut += 1
+        end
+        countIn += 1
+    end
+
+    # *sigh* Filehandling...
+    inp.close
+    out.close
+
+    # Generating Output
+    puts "3: Outputfile " + outName + " and " + countOut.to_s + " lines written"
 end
