aa3c887a1651647d7cde6251f16036794c91befa
Christopher Sauer <christopher.sauer@me.com>
2014-07-28 10:06:09 +0200

    corrected maximum line length

--- runrapidminer.m
+++ runrapidminer.m
@@ -1,5 +1,7 @@
 function [] = runrapidminer(pathToRapidminer, pathToProcess, processName)
-% call rapidminer from matlab, automagically - e.g.: runrapidminer('C:\Program Files\RapidMiner\RapidMiner Studio\scripts\','E:\Dropbox\University\Bachelor_7_2014-SS_BA\01_Bachelorarbeit\06_Parser\testing\','testprocess.rmp')
+% call rapidminer from matlab, automagically - e.g.:
+% runrapidminer('C:\Program Files\RapidMiner\RapidMiner Studio\scripts\', ...
+% 'E:\Dropbox\University\Bachelor_7_2014-SS_BA\01_Bachelorarbeit\06_Parser\testing\','testprocess.rmp')
 
 % e.g.
 %pathToRapidminer = 'C:\Program Files\RapidMiner\RapidMiner Studio\';
