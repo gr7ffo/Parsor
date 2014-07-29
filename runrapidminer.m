58abd93b5121224d4299d5fe26d82b0b4787b814
Christopher Sauer <christopher.sauer@me.com>
2014-07-29 09:11:12 +0200

    added standard file path for runrapidminer

--- runrapidminer.m
+++ runrapidminer.m
@@ -3,9 +3,10 @@ function [] = runrapidminer(pathToRapidminer, pathToProcess, processName)
 % runrapidminer('C:\Program Files\RapidMiner\RapidMiner Studio\scripts\', ...
 % 'E:\Dropbox\University\Bachelor_7_2014-SS_BA\01_Bachelorarbeit\06_Parser\testing\','testprocess.rmp')
 
-% e.g.
-%pathToRapidminer = 'C:\Program Files\RapidMiner\RapidMiner Studio\';
-%pathToProcess = 'E:\Dropbox\University\Bachelor_7_2014-SS_BA\01_Bachelorarbeit\06_Parser\testing\';
+% Standard file path
+if isempty(pathToRapidminer)
+   pathToRapidminer = 'C:\Program Files\RapidMiner\RapidMiner Studio\';
+end
 
 % adding script folder
 pathToRapidminer = [pathToRapidminer, 'scripts\']
